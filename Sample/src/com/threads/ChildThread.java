26b5e7748403d8bfc38b799802de382e23d2d26e
Bharaniarasu <bharaniarasu.t@gmail.com>
2021-06-25 19:23:24 +0530

    ..

--- Sample/src/com/threads/ChildThread.java
+++ Sample/src/com/threads/ChildThread.java
@@ -5,7 +5,7 @@ public class ChildThread extends Thread {
 		for (int i = 1; i <= 5; i++) {
 			// Thread.yield();
 			try {
-				Thread.sleep(100);
+				Thread.sleep(1000);
 				System.out.println("Child Thred " + i);
 			} catch (InterruptedException e) {
 				System.out.println("---Child Interrupted---");
